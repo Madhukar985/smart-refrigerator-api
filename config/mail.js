7c68f8cb14d5a1d65836de81b2f4daa851a68403
Smart Fridge Admin <smartrefrigeratorsystem1@gmail.com>
2026-03-20 17:28:23 +0530

    Fix Nodemailer ETIMEDOUT Render Block

--- config/mail.js
+++ config/mail.js
@@ -8,8 +8,12 @@ if (dns.setDefaultResultOrder) {
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false, // true for 465, false for other ports (starts TLS automatically)
+    requireTLS: true,
+    tls: {
+        rejectUnauthorized: false
+    },
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
