35457ea1e4d86f5a9d8ce9826676f3aa5fd7720e
Smart Fridge Admin <smartrefrigeratorsystem1@gmail.com>
2026-03-22 12:42:36 +0530

    Change cron schedule to 12:50 PM IST for testing

--- utils/cron.js
+++ utils/cron.js
@@ -92,8 +92,8 @@ const runExpiryCheck = async (userId = null) => {
     }
 }
 
-// Run every day at 9:00 AM (0 9 * * *)
-cron.schedule('0 9 * * *', () => runExpiryCheck(), {
+// Run for testing at 12:50 PM (50 12 * * *)
+cron.schedule('50 12 * * *', () => runExpiryCheck(), {
     timezone: "Asia/Kolkata"
 });
 
